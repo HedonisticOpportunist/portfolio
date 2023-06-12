3b31b2914c01e0eda433a17cec880bd41dd56d42
Hedonistic Opportunist <fujitahibaraki@gmail.com>
2023-06-12 18:38:06 +0100

    Some portfolio details added.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,31 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container, heading, navLinks, navLinkItem, navLinkText} from './layout.module.css'
+import { footer, container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <html lang='en'>
-    <body>
-    <div className={container}>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>Home</Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>About</Link></li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
-    </body>
+      <body>
+        <div className={container}>
+          <nav>
+            <ul className={navLinks}>
+              <li className={navLinkItem}>
+                <Link to="/" className={navLinkText}>Home</Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/about" className={navLinkText}>About</Link></li>
+            </ul>
+          </nav>
+          <main>
+            <h1 className={heading}>{pageTitle}</h1>
+            {children}
+          </main>
+        </div>
+        <div className={footer}>
+          <p>Author: Anita Pal</p>
+          <p><a href="mailto:fujitahibaraki@gmail.com">Contact Me ฅ/ᐠ. ̫ .ᐟ\ฅ </a></p>
+        </div>
+      </body>
     </html>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,16 @@ const IndexPage = () => {
         height="200"
         width="300"
       />
+      <p>👻 <a href="https://github.com/HedonisticOpportunist/drawing_app">Drawing Application</a> </p>
+      <p>A drawing application created for the University of London’s <a href="https://www.london.ac.uk/courses/introduction-programming-ii">Introduction to Programming II module. </a>
+      </p>
+      <p>A very ambitious and colourful project that demonstrates a passion for cats and emojis. </p>
+      <p>While some of the source code was provided by the lecturers, a lot of modifications were made to it, especially in regards to modifying the existing drawing tools. </p>
+      <p>👻 <a href="https://github.com/HedonisticOpportunist/cats_in_neath">Cats in Neath</a> </p>
+      <p>A website created for the University of London’s <a href="https://www.london.ac.uk/courses/web-development">Web Development module </a>
+        .</p>
+      <p>A very ambitious and colourful project that demonstrates a passion for cats and emojis. </p>
+      <p>A first foray into web development, with the content and images displaying love as well as passion for owning cats.  </p>
     </Layout>
   )
 }
